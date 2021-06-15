e3f33852b743baebb69f996106ffb4608b7d2a80
Kevin <pitzer.kevinj@gmail.com>
2021-06-15 09:15:14 -0400

    add axios

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World! From the index route.");
+  res.send(
+    `<p>Satellite stats endpoint: /api/satellite/stats</p><p>Satellite health endpoint: /api/satellite/health</p>`
+  );
 });
 
 module.exports = router;
